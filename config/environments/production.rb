6ae94f56acc5feb1308e6795c9eb6bef8057b329
John Seabolt <johnseabolt@Johns-MacBook-Pro.local>
2017-03-15 13:10:13 -0700

    restored

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,6 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
-  config.action_mailer.default_url_options = { :host => 'calcs4kids.herokuapp.com' }
   config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.delivery_method = :smtp
