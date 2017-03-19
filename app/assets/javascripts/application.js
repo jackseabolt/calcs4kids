16b452231f3d70896a664237d06e2a1b2f188c4f
John Seabolt <johnseabolt@Johns-MacBook-Pro.local>
2017-03-19 14:40:08 -0700

    social

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,12 +15,17 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+	
 
+	function drop() {
+		$('html, body').animate({ scrollTop: $(".section_5").offset().top}, 'slow');
+	}
 
 	function drop() {
 		    $('html,body').animate({
 		        scrollTop: $(".section_5").offset().top},'slow'); 
 		}
+
 
 
 	function parallex () {
@@ -33,41 +38,43 @@
 
 	$(document).on('turbolinks:load', function(){
 
-	function note () {
-		pos = window.pageYOffset;
-		if (pos > 300) {
-			$('#note').animate({opacity: '1'}, 1000);
-		} 
-	}
-	window.addEventListener('scroll', note);
 
 
-	function note_title () {
-		pos = window.pageYOffset;
-		if (pos > 300) {
-			$('#note_title').animate({opacity: '1'}, 1000);
-		} 
-	}
-	window.addEventListener('scroll', note_title);
-
-
-	function left() {
-		pos = window.pageYOffset; 
-		if (pos > 700) {
-			$('.left').animate({left: '0px'}, 1200);
+		function note () {
+			pos = window.pageYOffset;
+			if (pos > 300) {
+				$('#note').animate({opacity: '1'}, 1000);
+			} 
 		}
-	}
-	window.addEventListener('scroll', left);
+		window.addEventListener('scroll', note);
 
-	function right() {
-		pos = window.pageYOffset; 
-		if (pos > 1400) {
-			$('.right').animate({right: '0px'}, 1200);
+
+		function note_title () {
+			pos = window.pageYOffset;
+			if (pos > 300) {
+				$('#note_title').animate({opacity: '1'}, 1000);
+			} 
 		}
-	}
-	window.addEventListener('scroll', right);
+		window.addEventListener('scroll', note_title);
 
-});
+
+		function left() {
+			pos = window.pageYOffset; 
+			if (pos > 700) {
+				$('.left').animate({left: '0px'}, 1200);
+			}
+		}
+		window.addEventListener('scroll', left);
+
+		function right() {
+			pos = window.pageYOffset; 
+			if (pos > 1400) {
+				$('.right').animate({right: '0px'}, 1200);
+			}
+		}
+		window.addEventListener('scroll', right);
+
+    });
 	
 
 
